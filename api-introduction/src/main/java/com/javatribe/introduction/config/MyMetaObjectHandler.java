a7a57776c703db3aa8ed7113e82dbe4cfdc4a7cd
wzp520 <55944507+wzp520@users.noreply.github.com>
2020-11-22 20:24:05 +0800

    创建了部落介绍模块

---
+++ api-introduction/src/main/java/com/javatribe/introduction/config/MyMetaObjectHandler.java
@@ -0,0 +1,32 @@
+package com.javatribe.introduction.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author wzp
+ * @date 2020/11/22 19:59
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("insert fill...");
+        // 属性名称，不是字段名称
+        this.strictInsertFill(metaObject, "gmtCreate", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "gmtModified", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("update fill...");
+        this.strictUpdateFill(metaObject, "gmtModified", LocalDateTime.class, LocalDateTime.now());
+    }
+}
---
+++ api-introduction/src/main/java/com/javatribe/introduction/ddd.java
@@ -0,0 +1,8 @@
+package com.javatribe.introduction;
+
+/**
+ * @author wzp
+ * @date 2020/11/22 19:48
+ */
+public class ddd {
+}
