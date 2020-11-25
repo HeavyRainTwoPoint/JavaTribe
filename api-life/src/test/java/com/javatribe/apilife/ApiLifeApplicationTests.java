23e6bd0a35aed479c75417fe9b4ea55287d653fd
HeavyRainTwoPoint <1601219990@qq.com>
2020-11-25 15:42:31 +0800

    创建部落生活和技术分享两个模块

---
+++ api-life/src/test/java/com/javatribe/apilife/ApiLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatribe.apilife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ api-share/src/test/java/com/javatribe/apishare/ApiShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatribe.apishare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
