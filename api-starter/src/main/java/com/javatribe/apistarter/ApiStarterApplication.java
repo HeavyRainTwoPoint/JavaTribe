c72f65a3d0554c90d2a621d5b7b5be6329540a87
idea-your-father <1957476540@qq.com>
2021-03-07 15:19:08 +0800

    提交一下部署代码

--- api-starter/src/main/java/com/javatribe/apistarter/ApiStarterApplication.java
+++ api-starter/src/main/java/com/javatribe/apistarter/ApiStarterApplication.java
@@ -11,13 +11,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.javatribe")
-<<<<<<< HEAD
+// <<<<<<< HEAD
 // @MapperScan({"com.javatribe.apishare.mapper","com.javatribe.apicompetition.mapper"})
 
 // @MapperScan()
-=======
-@MapperScan({"com.javatribe.apishare.mapper", "com.javatribe.introduction.mapper"})
->>>>>>> 9d8a4a18581b6f94fa77c62de6634b3d29b70eea
+// =======
+@MapperScan({"com.javatribe.apishare.mapper", "com.javatribe.introduction.mapper","com.javatribe.apilife.dao"})
+// >>>>>>> 9d8a4a18581b6f94fa77c62de6634b3d29b70eea
 public class ApiStarterApplication {
 
     public static void main(String[] args) {
