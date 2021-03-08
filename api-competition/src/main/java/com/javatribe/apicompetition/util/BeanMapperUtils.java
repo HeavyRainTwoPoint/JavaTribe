da2dc714a66940c5633cc162830b2f21aaeeb64f
idea-your-father <1957476540@qq.com>
2021-03-08 20:32:53 +0800

    发布一下最新代码

--- api-competition/src/main/java/com/javatribe/apicompetition/util/BeanMapperUtils.java
+++ api-competition/src/main/java/com/javatribe/apicompetition/util/BeanMapperUtils.java
@@ -8,6 +8,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
+import java.util.List;
+
 /**
  * 属性拷贝
  * @Author lyr
@@ -50,4 +52,11 @@ public interface BeanMapperUtils {
 
     })
     StyleShow from(StyleShowDTO styleShowDTO);
+
+    //
+    // /**
+    //  * 数据库 对象批量转给 前端
+    //  * @return
+    //  */
+    // List<StyleShowDTO> styleShowEntity2DTO(List<StyleShow> list);
 }
